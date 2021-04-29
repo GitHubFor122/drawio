659697ad161c62971647123501b00202ee5d6ae4
Jin Wu <jin_kk666@163.com>
2021-04-29 09:56:22 +0800

    update

--- src/main/java/com/mxgraph/online/GitHubServlet.java
+++ src/main/java/com/mxgraph/online/GitHubServlet.java
@@ -104,9 +104,9 @@ public class GitHubServlet extends HttpServlet
 		{
 			String secret = client.equals("23bc97120b9035515661") ? DEV_CLIENT_SECRET : CLIENT_SECRET; 
 			String url = "https://github.com/login/oauth/access_token";
-			String githubURL = System.getenv("GITHUB_URL")
+			String githubURL = System.getenv("GITHUB_URL");
 			if(githubURL != null||githubURL!=""){
-				url = githubURL+"/login/oauth/access_token"
+				url = githubURL+"/login/oauth/access_token";
 			}
 			URL obj = new URL(url);
 			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
